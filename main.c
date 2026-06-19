659fe06845c74b2c09ad2fd4c9e7ae589338f5c7
Keiozzz <Darioalonso2007@gmail.com>
2026-06-19 14:33:16 -0400

    viernes en la mañana/ creacion mapa, lectura datos

--- main.c
+++ main.c
@@ -27,6 +27,12 @@ int interpretacion_datos(int mapa[][MAX], int *filas, int *columnas, int *naveX,
    int F = 0; 
    int C = 0;
    int cantidad_destructores = 0;
+   int cantidad_estrellas = 0;
+   int estrellaX, estrellaY;
+   char orientacion;
+   char ordenes[40];
+   int destructorX, destructorY;
+
    while (F < *filas) {
       C = 0;
       while (C < *columnas) {
@@ -41,14 +47,32 @@ int interpretacion_datos(int mapa[][MAX], int *filas, int *columnas, int *naveX,
 
    // 4. Leer posición del planeta T
    fscanf(archivo_entrada, "%d %d", planetaX, planetaY);
-   fscanf(archivo_entrada, "%d %d", estrellaX, estrellaY)
-   fscanf(archivo_entrada, "%d", &cantidad_destructores)
+   fscanf(archivo_entrada, "%d %d", &cantidad_estrellas);
+    
+   //crear funcion que ponga estrellas en el mapa
+   int i=0;
+   while(F < cantidad_estrellas){
+      fscanf(archivo_entrada, "%d" "%d", &estrellaX, &estrellaY);
+      while (i == estrellaX){
+      C = 0;
+      while (C == estrellaY){
+         mapa[F][C] = ESTRELLA;
+         C++;
+      }
+      F++;
+   }
+   }
+    
+   fscanf(archivo_entrada, "%d %d", estrellaX, estrellaY);
+   fscanf(archivo_entrada, "%d", &cantidad_destructores);
+   fscanf(archivo_entrada, "%d %d", destructorX, destructorY);
+   fscanf(archivo_entrada,"%c", orientacion);
 
    // 5. Colocar el planeta en el mapa
    mapa[*planetaX][*planetaY] = PLANETA;
 
    // Recordar cerrar el archivo al terminar esta función más adelante
-    / fclose(archivo_entrada); 
+   fclose(archivo_entrada); 
     
    return 0;
    fclose(archivo_entrada);
@@ -59,22 +83,46 @@ int interpretacion_datos(int mapa[][MAX], int *filas, int *columnas, int *naveX,
    FUNCION 2: SIMULACION DE LA NAVE
    ========================================== */
 int ruta_avion(int *naveX, int *naveY, char *orientacion, char ordenes[], int num_ordenes, int mapa[][MAX], int filas, int columnas) {
+
    int i = 0; 
-    
+
    while (i < num_ordenes) {
       // Corregido: se agregó la 'i' dentro de ordenes[i]
-      if (mapa[naveX][naveY] == DESTRUCTOR) {
-         printf("nave destruida\n"); 
+      if (mapa[*naveX][*naveY] == DESTRUCTOR){
+         return 1; 
       }
-      if (mapa[naveX][naveY] == ESTRELLA) {
-         printf("nave destruida\n");
+      if (mapa[*naveX][*naveY] == ESTRELLA){
+         return 1;
       }
-      if (mapa[naveY][naveY] == PLANETA) {
-         printf("llegamos a salvo\n");
+      if (mapa[*naveX][*naveY] == PLANETA){
+         return 2;
       }
       i = i + 1; 
+   
    }
-   return 0;
+   return 0;  // este return 0 significa que el avion se perdio implicitamente si no cumple ninguna de las 3
+
+}
+
+// FUNCION DE SALIDA
+
+void archivo_salida(int ruta_avion){
+   FILE*archivo_salida = fopen("situacion_final.txt", "w");;
+   if(archivo_salida == NULL){
+      printf("archivo  no identificado.\n");
+      return;    // QUITAR ANTES DE ENTREGAR!!!!
+   }
+
+   if(archivo_salida == 1){
+      fprintf(archivo_salida, "Nave destruida.\n");
+   }
+   if (archivo_salida == 2)
+   {
+      fprintf(archivo_salida,"" "Llegamos a salvo.");
+   }
+
+   
+   fclose(archivo_salida);
 }
 
 
@@ -85,7 +133,8 @@ int main() {
    int mapa[MAX][MAX];
    int filas;
    int columnas;
-   int num_Estrellas, num_Destructores;
+   int estrellasX, estrellasY, destructor;
    char orientacion;
    char max_mov;
-   int avionX, avion
+   int avionX, avionY;
+}
