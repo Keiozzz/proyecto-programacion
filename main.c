a3245edeba445f13ebb8426ca4620241eb9b21f4
Keiozzz <darioalonso2007@gmail.com>
2026-06-19 01:16:22 -0400

    noxecita de programacion

--- main.c
+++ main.c
@@ -1,39 +1,91 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+
 #define MAX 30
-#define MAX_MOV 40
+#define MAX_MOV 41
+#define VACIO 0
+#define NAVE 1
+#define ESTRELLA 2
+#define DESTRUCTOR 3
+#define PLANETA 4
 
-/*FUNCIONES AQUI*/
-void archivo_existe(FILE* archivo_entrada){
-        if(archivo_entrada == NULL){
-            exit(-1);
-        }    
-}
-/*CODIGO BASE*/
-
-/*el espacio por el que puede moverse la nave dividido en m filas (numeradas de 0 a m-1, yendo de N a S)
-y n columnas (numeradas de 0 a n-1, yendo de O a E)*/
-int main(){
-    int filas;
-    int columnas;
-    char max_mov;
-    int planetaX, PlanetaY;
-    int estrellaX, estrellaY;
-    int filas, columnas;
-    char ordenes [MAX_MOV];
-    FILE * archivo_entrada;
+/* ==========================================
+   FUNCION 1: ENTRADA DE ARCHIVO
+   ========================================== */
+int interpretacion_datos(int mapa[][MAX], int *filas, int *columnas, int *naveX, int *naveY, int *planetaX, int *planetaY, int estrellaX, int estrellaY) {
     
+   FILE *archivo_entrada = fopen("situacion_inicial.txt", "r");
+   if (archivo_entrada == NULL) {
+        exit(-1);    
+   }
+    
+   // 1. Leer dimensiones del mapa
+   fscanf(archivo_entrada, "%d %d", filas, columnas);
 
-    archivo_entrada = fopen("situacion_inicial.txt", "r");
-    archivo_existe(archivo_entrada);
-    fscanf(archivo_entrada, "%d%d", &filas,&columnas);
-    printf("%d %d",&filas, &columnas);
-    if(filas>=30 || columnas >=30){
-        return 1;
+   // 2. Inicializar todo el mapa en VACIO
+   int F = 0; 
+   int C = 0;
+   int cantidad_destructores = 0;
+   while (F < *filas) {
+      C = 0;
+      while (C < *columnas) {
+         mapa[F][C] = VACIO;
+         C++;
+      }
+      F++;
+   }
+    
+   // 3. Leer posición de la nave H
+   fscanf(archivo_entrada, "%d %d", naveX, naveY);
 
-    }
-   // fscanf(que quiero leer, que tipo de dato espera, donde lo guarda)
-    printf("seguimo vivo");
+   // 4. Leer posición del planeta T
+   fscanf(archivo_entrada, "%d %d", planetaX, planetaY);
+   fscanf(archivo_entrada, "%d %d", estrellaX, estrellaY)
+   fscanf(archivo_entrada, "%d", &cantidad_destructores)
 
+   // 5. Colocar el planeta en el mapa
+   mapa[*planetaX][*planetaY] = PLANETA;
+
+   // Recordar cerrar el archivo al terminar esta función más adelante
+    / fclose(archivo_entrada); 
+    
+   return 0;
+   fclose(archivo_entrada);
+} 
+
+
+/* ==========================================
+   FUNCION 2: SIMULACION DE LA NAVE
+   ========================================== */
+int ruta_avion(int *naveX, int *naveY, char *orientacion, char ordenes[], int num_ordenes, int mapa[][MAX], int filas, int columnas) {
+   int i = 0; 
+    
+   while (i < num_ordenes) {
+      // Corregido: se agregó la 'i' dentro de ordenes[i]
+      if (mapa[naveX][naveY] == DESTRUCTOR) {
+         printf("nave destruida\n"); 
+      }
+      if (mapa[naveX][naveY] == ESTRELLA) {
+         printf("nave destruida\n");
+      }
+      if (mapa[naveY][naveY] == PLANETA) {
+         printf("llegamos a salvo\n");
+      }
+      i = i + 1; 
+   }
+   return 0;
 }
+
+
+/* ==========================================
+   CODIGO BASE (MAIN)
+   ========================================== */
+int main() {
+   int mapa[MAX][MAX];
+   int filas;
+   int columnas;
+   int num_Estrellas, num_Destructores;
+   char orientacion;
+   char max_mov;
+   int avionX, avion
